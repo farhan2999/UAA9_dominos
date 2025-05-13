6bac7408dbe6e4a09af1f7e0312beb8e89762fd1
Abuzour Farhan <200550@CCM-Asty-Moulin.local>
2025-05-13 16:12:04 +0200

    13/05/2025

---
+++ JeuDominoConsole/Domino.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDominoConsole
+{
+    public struct Domino
+    {
+        public void GenererDominos()
+        {
+            int[,] dominos = new int[28,2];
+            int k = 0;
+            for (int j = 0; j <= 6; j++)
+            {
+                for (int i = j; i <= 6; i++)
+                {
+                    dominos[k, 0] = j;
+                    dominos[k,1] = i;
+                    k++;
+                }
+            }
+        }
+        public void Melanger()
+        {
+
+        }
+
+    }
+}
---
+++ JeuDominoConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace JeuDominoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Domino dom = new Domino();
+        }
+
+    }
+}
